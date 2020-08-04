e65260b799db533c1430d5d3a34c8e99b41a4931
amekusa <hello@amekusa.com>
2020-08-04 17:32:03 +0900

    + New methods, options for Exception

--- src/Exception.js
+++ src/Exception.js
@@ -1,3 +1,6 @@
+const truthy = Symbol('<Truthy>');
+let options;
+
 class Exception extends Error {
 	constructor(msg, info = null) {
 		super(msg + (info ? '\n> info: '+JSON.stringify(info, null, 2) : ''));
@@ -7,6 +10,39 @@ class Exception extends Error {
 	get info() {
 		return this._info;
 	}
+	trigger() {
+		if (typeof options.handler != 'function') throw this;
+		return options.handler(this);
+	}
+	static reset() {
+		options = {
+			handler: null
+		};
+		return Exception;
+	}
+	static option(name, value) {
+		if (arguments.length < 2) return options[name];
+		options[name] = value;
+		return Exception;
+	}
+	static options(set) {
+		if (!arguments.length) return Object.assign({}, options);
+		Object.assign(options, set);
+		return Exception;
+	}
+	static failed(checked, expected) {
+		return new Exception(`unexpected value`, {
+			checked,
+			expected
+		});
+	}
+	static check(value, expected = truthy) {
+		if (arguments.length < 2 && value) return value;
+		if (value === expected) return value;
+		return Exception.failed(value, truthy).trigger();
+	}
 }
+
+Exception.reset();
 
 export default Exception;
--- src/InvalidType.js
+++ src/InvalidType.js
@@ -2,24 +2,22 @@ import Exception from './Exception.js';
 
 class InvalidType extends Exception {
 	constructor(info = null) {
-		super('invalid type of value detected', info);
+		super('unexpected type of value detected', info);
+	}
+	static failed(checkedValue, expectedType, actualType = null) {
+		return new InvalidType({
+			checkedValue,
+			expectedType,
+			actualType: actualType || typeof checkedValue
+		});
+	}
+	static check(value, expectedType) {
+		let actualType = typeof value;
+		if (typeof expectedType == 'function' && actualType == 'object') {
+			if (value instanceof expectedType) return true;
+		} else if (actualType == expectedType) return true;
+		return InvalidType.failed(value, expectedType, actualType).trigger();
 	}
 }
-
-InvalidType.failed = function (checkedValue, expectedType, actualType = null) {
-	return new InvalidType({
-		checkedValue,
-		expectedType,
-		actualType: actualType || typeof checkedValue
-	});
-};
-
-InvalidType.check = function (value, expectedType) {
-	let actualType = typeof value;
-	if (typeof expectedType == 'function' && actualType == 'object') {
-		if (value instanceof expectedType) return true;
-	} else if (actualType == expectedType) return true;
-	throw InvalidType.failed(value, expectedType, actualType);
-};
 
 export default InvalidType;
