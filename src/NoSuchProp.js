8fd0cf57316e5ab14f82b23e080f1dbcb14ec223
amekusa <hello@amekusa.com>
2020-11-24 05:11:11 +0900

    + New exception: NoSuchProp

---
+++ src/NoSuchProp.js
@@ -0,0 +1,38 @@
+import Exception from './Exception.js';
+
+/**
+ * Thrown to indicate that an object doesn't have expected properties.
+ * @extends Exception
+ * @hideconstructor
+ */
+class NoSuchProp extends Exception {
+	/**
+	 * Checks if `prop` is expected by this exception.
+	 * @param {string} prop Name of a property to check
+	 * @return {boolean} `true` if `prop` is expected
+	 */
+	expects(prop) {
+		return super.expects(prop);
+	}
+	static failed(obj, ...prop) {
+		return new this(`expected properties missing`, {
+			checked: obj,
+			expected: prop.length > 1 ? prop : prop[0]
+		});
+	}
+	/**
+	 * Checks if `obj` has `prop` as its property. If it doesn't, [triggers]{@link NoSuchProp#trigger} an exception.
+	 * Otherwise, just returns `obj`.
+	 * @param {object} obj The object to check
+	 * @param {...string} prop The property name to check if it exists
+	 * @return {object} Just returns the `obj` argument if there's no problem
+	 */
+	static check(obj, ...prop) {
+		for (let I of prop) {
+			if (obj[I] !== undefined) return obj;
+		}
+		return this.failed(obj, ...prop).trigger();
+	}
+}
+
+export default NoSuchProp;
