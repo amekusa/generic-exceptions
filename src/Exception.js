4b042768f7a097897ea3afab31cc1d208174954c
amekusa <hello@amekusa.com>
2021-01-29 20:13:14 +0900

    + Default message

--- src/Exception.js
+++ src/Exception.js
@@ -10,7 +10,7 @@ class Exception extends Error {
 	 * @param {boolean} [hidesInfo=false] Whether or not to hide `info`
 	 */
 	constructor(msg = null, info = null, hidesInfo = false) {
-		super((msg ? format(msg, info) : '') + ((info && !hidesInfo) ? `\n[${new.target.name}] To debug, catch me and see .info: ${JSON.stringify(info, null, 2)}` : ''));
+		super((msg ? format(msg, info) : format(new.target.message)) + ((info && !hidesInfo) ? `\n[${new.target.name}] To debug, catch me and see .info: ${JSON.stringify(info, null, 2)}` : ''));
 		this.name = new.target.name;
 		this._info = info;
 	}
@@ -21,6 +21,14 @@ class Exception extends Error {
 	 */
 	get info() {
 		return this._info;
+	}
+	/**
+	 * The default message for this exception class.
+	 * @type {string}
+	 * @readonly
+	 */
+	static get message() {
+		return '';
 	}
 	/**
 	 * Throws this exception if `handler` option is not set.
